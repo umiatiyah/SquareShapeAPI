3fdfed2e6d3cb657673c4e9d56a735432899b826
umi.atiyah <umi.atiyah@axa-mandiri.co.id>
2023-12-28 23:53:59 +0700

    create API Change Color

---
+++ SquareShapeAPI/Controllers/ColorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SquareShapeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ColorController : ControllerBase
+    {
+        private readonly ILogger<ColorController> _logger;
+
+        public ColorController(ILogger<ColorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("changeColor")]
+        public ActionResult ChangeColor([FromBody] string color)
+        {
+            return Ok($"Color successfully changed to {color}");
+        }
+    }
+}
